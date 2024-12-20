1894b993ded593a4aed4aa67903145f159dc4834
Jason Wild <wildj@wwu.edu>
2024-12-19 21:48:30 -0800

    Added Formik form with Yup validation

--- src/pages/expense/NewExpense.tsx
+++ src/pages/expense/NewExpense.tsx
@@ -1,8 +1,107 @@
+import { useFormik } from "formik";
+import { Expense } from "../../model/Expense";
+import expenseValidationSchema from "../../validation/ExpenseValidationSchema";
 
 const NewExpense = () => {
-  return (
-    <div>NewExpense</div>
-  )
-}
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      amount: 0,
+      note: "",
+      category: "",
+      date: new Date().toISOString().split("T")[0],
+    },
+    onSubmit: (values: Expense) => {
+      console.log(values);
+    },
+    validationSchema: expenseValidationSchema,
+  });
 
-export default NewExpense
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center mt-2">
+        <div className="container col-md-4 col-sm-8 col-xs 12">
+          <form onSubmit={formik.handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="form-control"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.name && formik.errors.name ? (
+                <div className="text-danger fst-italic">
+                  {formik.errors.name}
+                </div>
+              ) : null}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="amount" className="form-label">
+                Amount
+              </label>
+              <input
+                type="number"
+                id="amount"
+                name="amount"
+                className="form-control"
+                value={formik.values.amount}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.amount && formik.errors.amount ? (
+                <div className="text-danger fst-italic">
+                  {formik.errors.amount}
+                </div>
+              ) : null}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="note" className="form-label">
+                Description
+              </label>
+              <textarea
+                id="note"
+                name="note"
+                className="form-control"
+                value={formik.values.note}
+                onChange={formik.handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="date" className="form-label">
+                Date
+              </label>
+              <input
+                type="date"
+                id="date"
+                name="date"
+                className="form-control"
+                value={formik.values.date}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.date && formik.errors.date ? (
+                <div className="text-danger fst-italic">
+                  {formik.errors.date}
+                </div>
+              ) : null}
+            </div>
+            <button
+              className="btn btn-sm btn-primary btn-outline-light"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NewExpense;
---
+++ src/validation/ExpenseValidationSchema.ts
@@ -0,0 +1,9 @@
+import * as Yup from 'yup';
+
+const expenseValidationSchema = Yup.object({
+    name: Yup.string().min(3, "Expense name must be at least 3 characters long").required('Expense name is required'),
+    amount: Yup.number().required("Expense amount is required").positive("Expense amount must be greater than zero"),
+    date: Yup.date().required("Expense date is required")
+  })
+
+  export default expenseValidationSchema
